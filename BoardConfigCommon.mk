9b4803f84ccbda1d25c91f922557e54d34f895f2
Tim Zimmermann <tim@linux4.de>
2020-10-18 21:37:07 +0200

    universal9611: build and flash a disabled vbmeta image
    A combination of --flags 0 and --algorithm NONE
    disables avb verification through bootloader
    while --flags 2 and --set_hastree_disabled_flag
    lead to reboot to download mode with vbmeta error
    
    Change-Id: Ia7860243c1d14cfc8ac54b9801802866d88f47a1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -178,6 +178,8 @@ $(call soong_config_set,samsungUsbGadgetVars,gadget_name,13200000.dwc3)
 
 ## Verified Boot
 BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 0
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --algorithm NONE
 BOARD_AVB_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
 BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
