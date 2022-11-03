a68ea519d351a01f20bd36400b73f1f13849b33e
LuK1337 <priv.luk@gmail.com>
2022-11-03 20:57:11 +0100

    universal9611: Add hardware/samsung to soong namespaces
    See I78b1b9930a57bc01afda722b9f8a196c372f9d8b.
    
    Change-Id: I634b562e12b70a0c060d449d3ba1d4e87204cf5d

--- common.mk
+++ common.mk
@@ -266,6 +266,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
+    hardware/samsung \
     hardware/samsung/aidl/power-libperfmgr
 
 # Shims
