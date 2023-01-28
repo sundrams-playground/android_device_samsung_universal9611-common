c60e3593d3cf5f6cc20a9d732af9f7aabfa18151
roynatech2544 <whiteshell2544@naver.com>
2023-01-28 16:19:20 +0900

    universal9611: camera: Drop gcamgo
    Most ROMs ship aperture now and it is better

--- common.mk
+++ common.mk
@@ -61,8 +61,7 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5-service_64 \
-    libsensorndkbridge \
-    GCameraGoPrebuilt
+    libsensorndkbridge
 
 # ConfigStore
 PRODUCT_PACKAGES += \
