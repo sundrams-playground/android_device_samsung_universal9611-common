ca7e75f9bb1761f21f6d7c794db6a15b2995868d
Cat658011 <cat658011@gmail.com>
2024-06-16 05:47:02 +0500

    universal9611: vintf: Move Samsung related HALs to device framework matrix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -113,6 +113,7 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE := false
 
 ## Manifest
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(COMMON_PATH)/device_framework_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
