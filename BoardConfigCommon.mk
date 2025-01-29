c9e689b838b24130b3fa8ecd52050df9ef45b9e3
Cat658011 <cat658011@gmail.com>
2025-01-29 05:27:11 +0500

    Revert "universal9611: Silence clang warnings during kernel compile"
    This reverts commit f70134ccf558957c57d544c64cc8ef70c25b92b3.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -99,7 +99,6 @@ TARGET_USES_VULKAN := true
 
 ## Kernel
 BOARD_KERNEL_IMAGE_NAME := Image
-TARGET_KERNEL_ADDITIONAL_FLAGS := HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 ## Keymaster
 TARGET_KEYMASTER_VARIANT := samsung
